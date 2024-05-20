460f566977320969766b5bc689b22ecdee48d740
irwan.azam@cloud-connect.asia <irwan.azam@cloud-connect.asia>
2024-05-20 13:39:18 +0800

    First initial

---
+++ MinimalWebApi/BasicAuthentication/Authentications/Post/Login.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BasicAuthentication.Authentication;
+
+    public record User
+    {
+        public string Username { get; set;}
+        public string Password { get; set;}
+    }
+
+    public static class LoginEndPoints
+    {
+        public static void Features(WebApplication app)
+        {
+            app.MapPost("/login",([FromBody]User user)=>{
+                //Console.WriteLine(user.UserName);
+                //return TypedResults.Ok(user.UserName);
+                
+                if (user.Username == "test" && user.Password == "password")
+                {
+                    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Your_Secret_Key_12345678910111213"));
+
+                    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                    var claims = new[]
+                        {
+                            new Claim(JwtRegisteredClaimNames.Sub, "test token"),
+                            new Claim(JwtRegisteredClaimNames.Name, "test"),
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                        };
+
+                    var token = new JwtSecurityToken(
+                    issuer: "http://localhost:5231",
+                    audience: "test-aud",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(120),
+                    signingCredentials: credentials);
+
+                    string tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                    //return TypedResults.Ok(new { Token = tokenString });
+                    return Results.Ok(new {Token = tokenString});
+                   
+                }
+
+            return TypedResults.Ok();
+            
+            });
+        }
+    }
---
+++ MinimalWebApi/BasicAuthentication/Program.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using BasicAuthentication.Authentication;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var key = Encoding.ASCII.GetBytes("Your_Secret_Key_12345678910111213");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        SaveSigninToken = true,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "http://localhost:5231",
+        ValidAudience = "test-aud",
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+// var summaries = new[]
+// {
+//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+// };
+
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast =  Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast")
+// .WithOpenApi();
+
+LoginEndPoints.Features(app);
+
+app.MapGet("/secure", () => "This is a secure endpoint").RequireAuthorization();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
